ffe0c58038add7f4bd7d76c5eafc151f9dd3cd5f
lukeathh <athertonluke052@gmail.com>
2024-07-16 14:44:38 +0100

    drinky

--- water-app/src/components/Header.jsx
+++ water-app/src/components/Header.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import "./Header.css"
+import React from 'react';
+import "./Header.css";
+import logo from "../assets/Sink.gif";
 const Header = () => {
   return (
-
-  <>   
-    <div className='cont'>Header</div>
-  </> 
-   )
+    <>
+      <div className='cont1'>
+        <img src={logo} alt="Logo" id="logo" />
+        <h1 className="title">Drinkify</h1>
+      </div>
+      
+    </>
+  );
 }
-
-export default Header
+export default Header;
