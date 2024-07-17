19d38c8b2c4ed99117e2928d1d124ffc28158987
marklovejoydev <marklovejoydev@gmail.com>
2024-07-17 12:04:51 +0100

    initial push notifiaction

--- water-app/src/App.jsx
+++ water-app/src/App.jsx
@@ -1,17 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import './App.css'
 import Hydration from './components/Hydration'
 import RecentlyAdded from './components/RecentlyAdded'
 import WaterLogs from './components/WaterLogs'
 import Footer from './components/Footer'
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Controls from './components/Controls'
+import { generateToken, messaging } from './notifications/firebase'
+import { onMessage } from 'firebase/messaging'
+
 
 function App() {
   // Initialise intakes state
   const [intakes, setIntakes] = useState([]);
+
+useEffect(() =>{
+  generateToken();
+  onMessage(messaging, (payload) => {
+    console.log(payload)
+  })
+}, [])
 
   // Function to add a new intake
   const addIntake = (intake) => {
